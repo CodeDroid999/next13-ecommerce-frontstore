b0afd5e8f8842ece31d32b8e1b713d9bfb70a718
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-22 12:46:48 +0300

    zangu zimeshiska Myy girl

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
@@ -8,12 +9,14 @@ import getCategories from "@/actions/get-categories";
 const Navbar = async () => {
   const categories = await getCategories();
 
-  return ( 
+  return (
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">STORE</p>
+            <p className="font-bold text-xl">
+              <Image className="m-1" src="https://i.postimg.cc/6py1Pcdf/computershop-logo.png" alt="Logo" width="50" height="50"></Image>
+            </p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
@@ -22,5 +25,5 @@ const Navbar = async () => {
     </div>
   );
 };
- 
+
 export default Navbar;
